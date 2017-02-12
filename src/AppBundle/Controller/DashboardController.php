d1c8347debf15b438a9e7129d2cfb04df5c63b96
hank.kam <hank.kam@gmail.com>
2017-02-12 08:26:40 +0000

    Enable dependency injection

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -4,18 +4,23 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class DashboardController
 {
     /** @var \Symfony\Bundle\FrameworkBundle\Templating\EngineInterface */
     private $templating;
 
+    /** @var \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage */
+    private $tokenStorage;
+
     /**
      * @param \Symfony\Bundle\FrameworkBundle\Templating\EngineInterface $templating
+     * @param \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage $tokenStorage
      */
-    public function __construct(EngineInterface $templating)
-    {
+    public function __construct(EngineInterface $templating, TokenStorage $tokenStorage) {
         $this->templating = $templating;
+        $this->tokenStorage = $tokenStorage;
     }
 
     /**
@@ -25,6 +30,11 @@ class DashboardController
      */
     public function indexAction(Request $request)
     {
-        return $this->templating->renderResponse('AppBundle:dashboard:index.html.twig');
+        dump($this->tokenStorage->getToken()->getUser());
+        $data = array(
+            'username' => $this->tokenStorage->getToken()->getUser()->getUserName()
+        );
+
+        return $this->templating->renderResponse('AppBundle:dashboard:index.html.twig', $data);
     }
 }
